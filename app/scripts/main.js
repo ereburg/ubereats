bb1c6ea6b5751babb1115f98df36ce87cc9b70d7
Eugene <eugene.kotsarev@gmail.com>
2019-11-17 01:46:28 +0300

    update 1.1 add filter script; add new page for restraunt; minor improvements;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-	// Custom JS
-	console.log('Hello, World!');
+	// Filter
+
+	let input = document.querySelector('#search-input');
+	// let	kitchen = document.querySelectorAll(".stores__description");
+	
+	input.addEventListener('keyup', () => {
+		let filter, wrapper, store, name, kitchen, txtValue;
+		filter = input.value.toUpperCase();
+		wrapper = document.querySelector(".stores__wrapper");
+		store = wrapper.querySelectorAll('.stores__item');
+		
+		for (let i = 0; i < store.length; i++) {
+			name = store[i].querySelectorAll(".stores__heading")[0];
+			kitchen = store[i].querySelectorAll(".stores__description")[0]; 
+			txtValue = name.textContent || name.innerText || kitchen.textContent || kitchen.innerText; console.log(kitchen.textContent);
+			if (txtValue.toUpperCase().indexOf(filter) > -1) {
+				store[i].style.display = "";
+			} else {
+				store[i].style.display = "none";
+			}
+		}
+	});
+
+	// input.addEventListener('keydown', myFilterFunction());
 });
