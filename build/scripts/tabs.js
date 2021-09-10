777f914a4b39234444b6a23b0ed52c37beaf8a78
ereburg <gimmethenick@gmail.com>
2021-09-10 23:34:43 +0300

    upd repo

--- build/scripts/tabs.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll(".tab-button").forEach(t=>{t.addEventListener("click",(function(){let e=this.getAttribute("data-tab"),a=document.querySelector('.tabcontent[data-tab="'+e+'"]'),c=document.querySelector(".tab-button.active"),n=document.querySelector(".tabcontent.active");c.classList.remove("active"),t.classList.add("active"),n.classList.remove("active"),a.classList.add("active")}))})});
-//# sourceMappingURL=tabs.js.map
